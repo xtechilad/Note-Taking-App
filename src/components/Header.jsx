572df9f2ce757b252dfa050b88e158b1373a8e28
Priyanshu Sharma <ps940016@gmail.com>
2022-10-08 21:58:51 +0530

    Refactor

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header() {
     return (
         <header className='py-5 px-2 xl:px-20 2xl:px-44 text-white bg-[#294aae] flex'>
-            <a href="https://github.com/xtechilad/note-taking-app" target="#">
+            <a href="https://github.com/xtechilad/note-taking-app" target="_blank" rel="noreferrer">
                 <h1 className='text-[1.7rem] ml-4 xl:ml-0 overflow-hidden'>NoteUs</h1>
             </a>
         </header>
